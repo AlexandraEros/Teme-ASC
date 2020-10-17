8c49264f6757b3ebee83085d0e06d4975cc8ca91
AlexandraEros <72766735+AlexandraEros@users.noreply.github.com>
2020-10-17 16:47:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Legea_lui_Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            uint n; //natural
+
+            Console.WriteLine(" Introduceti un numar natural n, pentru a afla in cat timp tranzistoarele vor deveni  de n-ori mai puterice fata de zilele noastre:");
+            while (true)
+            {
+                Console.Write("Introduceti un numar natural:");
+                input = Console.ReadLine();
+                //folosim TryParse pentru a consulta daca input-ul este un numar natural, sau nu.
+                if (uint.TryParse(input, out n))
+                {
+                    double ani; // real
+                    ani = 1.5 * Math.Log(n, 2);
+                    Console.WriteLine("Tranzistoarele vor fi mai rapide de " + n + " ori in " + ani + " ani");
+                    Console.WriteLine("Tranzistoarele vor fi mai rapide de {0} ori in {1} ani", n, ani);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Introduceti un numar natural!\n");
+                    //se va repeta pana cand utilizatorul introduce un numar natural.
+                }
+            }
+        }
+    }
+}
